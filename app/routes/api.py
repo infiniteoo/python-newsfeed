e08c7d34a7a40a93fd8bb91de7f3551cc08e3563
Troy Dorman <troydorman@gmail.com>
2021-04-05 18:46:14 -0600

    added error handling to api routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
@@ -9,16 +10,21 @@ def signup():
   data = request.get_json()
   db = get_db()
 
-  # create a new user
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+  try:
+    # attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  # save in database
-  db.add(newUser)
-  db.commit()
+    db.add(newUser)
+    db.commit()
+  except:
+    # insert failed, so send error to front end
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
 
   return jsonify(id = newUser.id)
 
